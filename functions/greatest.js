89429f4c95705b8981e2614da68b8d316c318add
shawazi <shawazisonfire@gmail.com>
2022-12-18 12:52:06 -0500

    updated conditional arrangement for proper functionality, but function still shows 'undefined' under correct output in console

--- functions/greatest.js
+++ functions/greatest.js
@@ -1,9 +1,11 @@
-num1 = +prompt("Enter first value: ");
-num2 = +prompt("Enter second value: ");
-num3 = +prompt("Enter third value: ");
+num1 = prompt("Enter first value: ");
+num2 = prompt("Enter second value: ");
+num3 = prompt("Enter third value: ");
 
 function greatest(num1, num2, num3) {
-    if (num1 > num2 && num1 > num3) {
+    if (isNaN(num1 || num2 || num3) === true) {
+        return console.log("Please enter a number for each value.")
+    } else if (num1 > num2 && num1 > num3) {
         return console.log(`${num1} is the greatest value`)
     } else if (num2 > num1 && num2 > num3) {
         return console.log(`${num2} is the greatest value`)
@@ -17,9 +19,13 @@ function greatest(num1, num2, num3) {
         return console.log(`${num1} and ${num3} are tied for the greatest value`)
     } else if (num1 === num2) {
         return console.log(`${num1} and ${num2} are tied for the greatest value`)
-    } else {
-        return console.log("Please enter a number for each value.")
     }
 }
 
 console.log(greatest(num1, num2, num3))
+
+// https://stackoverflow.com/questions/17907455/how-to-get-numeric-value-from-a-prompt-box this appears to solve my issue with strings being converted to 0. 
+
+// okay, i just had to rearrange one more set of conditional statements in order to get the functionality i need. But the console is still outputting "undefined" underneath the correct output for each test case. 
+
+// i think i am still misusing the return statements. 
